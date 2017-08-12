ac9da908befcdf13969cabbcd248fb5d2a9f6e9c
Eric D'Urso <eric@casadurso.com>
2017-08-12 19:57:53 -0400

    Great Progress, I have started working on autonomous code, unsure of color sensor return values, we will need to test for them soon, right now it just moves a servo if it sees the color white.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoDemoGallimore.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoDemoGallimore.java
@@ -11,15 +11,15 @@ import com.qualcomm.robotcore.hardware.Servo;
  */
 
 public class AutoDemoGallimore extends LinearOpMode {
-    //Identify Variables
-    private boolean Line;
+    //Identify Variables if any/needed
     //Identify Motors
     private DcMotor NorthMotor;
     private DcMotor SouthMotor;
     private DcMotor EastMotor;
     private DcMotor WestMotor;
-    private Servo RedArm;
-    private Servo BlueArm;
+    //private Servo RedArm;
+    //private Servo BlueArm;
+    private Servo iSeeWhite;
     private com.qualcomm.robotcore.hardware.ColorSensor ColorSensor;
 
     public AutoDemoGallimore (){
@@ -33,22 +33,40 @@ public class AutoDemoGallimore extends LinearOpMode {
         SouthMotor = hardwareMap.dcMotor.get("SouthDrive");
         EastMotor = hardwareMap.dcMotor.get("EastDrive");
         WestMotor = hardwareMap.dcMotor.get("WestDrive");
-        //Set Up Servo arms
-        RedArm = hardwareMap.servo.get("Red Arm");
-        BlueArm = hardwareMap.servo.get("Blue Arm");
+        //Set Up Servo arms                    !!!WE WOULD USE THESE ONCE WE LEARN MORE ABOUT THE COLOR SENSOR
+        // RedArm = hardwareMap.servo.get("Red Arm");
+        //BlueArm = hardwareMap.servo.get("Blue Arm");
+        iSeeWhite = hardwareMap.servo.get("I See White Sign");
         //Set Up Color Sensor
         ColorSensor = hardwareMap.colorSensor.get("Color Sensor");
 
-        Line = false;
+       iSeeWhite.setPosition(0.001);
 
         //Start moving
-        NorthMotor.setPower(.5);
-        SouthMotor.setPower(-.5);
 
-        while (){
-
+        while (ColorSensor.alpha() < 20){     // This would make it stop at a white line i think
+            NorthMotor.setPower(.5);
+            SouthMotor.setPower(-.5);
         }
 
+        STOP_MOTORS();
+        RAISE_SIGN();
+
+
+
+    }
+    // Create Methods Below
+    public void STOP_MOTORS (){
+        NorthMotor.setPower(0);
+        SouthMotor.setPower(0);
+        EastMotor.setPower(0);
+        WestMotor.setPower(0);
+    }
+
+    public void RAISE_SIGN (){
+        iSeeWhite.setPosition(0.65);
+        sleep(6000);
+        iSeeWhite.setPosition(0.001);
     }
 
 }
