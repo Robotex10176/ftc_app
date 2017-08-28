295840379f00d35d0d8bdd390bc200aacc92a1a3
Eric D'Urso <eric@casadurso.com>
2017-08-28 15:35:16 -0400

    SOMETHING AWESOME WORKS

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AUTOLINESEEIN.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AUTOLINESEEIN.java
@@ -15,24 +15,50 @@ public class AUTOLINESEEIN extends LinearOpMode {
     private DcMotor WpEn;
     private DcMotor SpNn;
     private DcMotor NpSn;
-    private int DIRECTION = 1;
+    private int VERT_DIRECTION = 1;
     private double WANTED_PWR = 0.15;
-    private double PWR = DIRECTION*WANTED_PWR;
+    private double VERT_PWR = VERT_DIRECTION*WANTED_PWR;
+    private int HOR_DIRECTION = 1;
+    private double HOR_PWR = HOR_DIRECTION*WANTED_PWR;
+    private int NO_POW = 0;
     OpticalDistanceSensor ODS;
     @Override
     public void runOpMode() throws InterruptedException {
         ODS = hardwareMap.opticalDistanceSensor.get("ODS");
         EpWn = hardwareMap.dcMotor.get("EpWn");
-        WpEn = hardwareMap.dcMotor.get("WpEn");//
+        WpEn = hardwareMap.dcMotor.get("WpEn");
         SpNn = hardwareMap.dcMotor.get("SpNn");
-        NpSn = hardwareMap.dcMotor.get("NpSn");//
+        NpSn = hardwareMap.dcMotor.get("NpSn");
         WpEn.setDirection(DcMotor.Direction.REVERSE);
         NpSn.setDirection(DcMotor.Direction.REVERSE);
         waitForStart();
         while (opModeIsActive()){
+            VERT_PWR = VERT_DIRECTION*WANTED_PWR;
+            HOR_PWR = HOR_DIRECTION*WANTED_PWR;
             while (ODS.getRawLightDetected()<1.25){
-
+                EpWn.setPower(HOR_PWR);
+                WpEn.setPower(HOR_PWR);
+                SpNn.setPower(VERT_PWR);
+                NpSn.setPower(VERT_PWR);
             }
+            EpWn.setPower(NO_POW);
+            WpEn.setPower(NO_POW);
+            SpNn.setPower(NO_POW);
+            NpSn.setPower(NO_POW);
+            VERT_DIRECTION = -VERT_DIRECTION;
+            HOR_DIRECTION = -HOR_DIRECTION;
+            VERT_PWR = VERT_DIRECTION*WANTED_PWR;
+            HOR_PWR = HOR_DIRECTION*WANTED_PWR;
+            EpWn.setPower(HOR_PWR);
+            WpEn.setPower(HOR_PWR);
+            SpNn.setPower(VERT_PWR);
+            NpSn.setPower(VERT_PWR);
+            sleep(500);
+            EpWn.setPower(NO_POW);
+            WpEn.setPower(NO_POW);
+            SpNn.setPower(NO_POW);
+            NpSn.setPower(NO_POW);
+
         }
     }
 }
