8ed774c2587ba300819ec6b5a2fdfa3499d0004d
hacker <github-f@standland.org>
2017-08-29 16:00:56 -0400

    meeting 8/29/17

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GALLIMORE_TELEOP_V1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GALLIMORE_TELEOP_V1.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode;
+package org.firstinspires.ftc.teamcode ;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
@@ -30,17 +30,21 @@ public class GALLIMORE_TELEOP_V1 extends OpMode {
 
     @Override
     public void loop() {
-
-        EpWn.setPower(scaleController(-gamepad1.right_stick_x));
-        WpEn.setPower(scaleController(gamepad1.right_stick_x));
-        SpNn.setPower(scaleController(gamepad1.left_stick_y));
-        NpSn.setPower(scaleController(-gamepad1.left_stick_y));
-        //TODO add conditional below
-        //EpWn.setPower(scaleController(gamepad1.right_trigger));
-        //WpEn.setPower(scaleController(gamepad1.right_trigger));
-        //SpNn.setPower(scaleController(gamepad1.left_trigger));
-        //NpSn.setPower(scaleController(gamepad1.left_trigger));
-
+        //primary driving block
+        if ((gamepad1.right_trigger < 0.05 )&&(gamepad1.left_trigger < 0.05 )) {
+            EpWn.setPower(scaleController(-gamepad1.right_stick_x));
+            WpEn.setPower(scaleController(gamepad1.right_stick_x));
+            SpNn.setPower(scaleController(gamepad1.left_stick_y));
+            NpSn.setPower(scaleController(-gamepad1.left_stick_y));
+        }
+        //done add conditional below
+        if ((java.lang.Math.abs(gamepad1.right_stick_x) < 0.05)&&(java.lang.Math.abs(gamepad1.left_stick_x) < 0.05)) {
+            //this block rotates the robot right
+            EpWn.setPower(scaleController(gamepad1.right_trigger));
+            WpEn.setPower(scaleController(gamepad1.right_trigger));
+            SpNn.setPower(scaleController(gamepad1.right_trigger));
+            NpSn.setPower(scaleController(gamepad1.right_trigger));
+        }
 
 
     }
