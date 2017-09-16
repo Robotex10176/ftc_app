e7038e0ed17ebbd85f990e3596b370f4e3c5c214
Eric D'Urso <eric@casadurso.com>
2017-09-16 14:39:17 -0400

    Copied Vuforia ID and broke down AUTO

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Red_TeleOp_1.java
@@ -0,0 +1,31 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+/**
+ * Created by Eric D'Urso on 9/16/2017.
+ */
+
+public class Red_TeleOp_1 extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        waitForStart();
+        KnockOffJewl();
+        ScanVuMark();
+        DriveToSafeZone();
+        PlaceGlyph();
+    }
+    public void KnockOffJewl(){
+
+    }
+    public void ScanVuMark(){
+
+    }
+    public void DriveToSafeZone(){
+
+    }
+    public void PlaceGlyph(){
+
+    }
+}
+
