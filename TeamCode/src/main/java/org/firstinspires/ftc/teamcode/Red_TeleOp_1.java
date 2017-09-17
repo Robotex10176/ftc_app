ccdf87b211c4b79aa125422769a67089920b204b
Eric D'Urso <eric@casadurso.com>
2017-09-17 10:57:52 -0400

    removed unnessary comments

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Red_TeleOp_1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Red_TeleOp_1.java
@@ -55,42 +55,24 @@ public class Red_TeleOp_1 extends LinearOpMode {
 
         ranVuforia = false;
         telemetry.addData("Vuforia Status:", ranVuforia);
+        sleep(2000);
 
         relicTrackables.activate();
 
         while (opModeIsActive()) {
-            /**
-             * See if any of the instances of {@link relicTemplate} are currently visible.
-             * {@link RelicRecoveryVuMark} is an enum which can have the following values:
-             * UNKNOWN, LEFT, CENTER, and RIGHT. When a VuMark is visible, something other than
-             * UNKNOWN will be returned by {@link RelicRecoveryVuMark#from(VuforiaTrackable)}.
-             */
+            //all of this code is in COnceptVuMarkIdentification.java
             RelicRecoveryVuMark vuMark = RelicRecoveryVuMark.from(relicTemplate);
             if (vuMark != RelicRecoveryVuMark.UNKNOWN) {
-
-                /* Found an instance of the template. In the actual game, you will probably
-                 * loop until this condition occurs, then move on to act accordingly depending
-                 * on which VuMark was visible. */
                 telemetry.addData("VuMark", "%s visible", vuMark);
-
-                /* For fun, we also exhibit the navigational pose. In the Relic Recovery game,
-                 * it is perhaps unlikely that you will actually need to act on this pose information, but
-                 * we illustrate it nevertheless, for completeness. */
+                //They also use the navigation portion
                 OpenGLMatrix pose = ((VuforiaTrackableDefaultListener)relicTemplate.getListener()).getPose();
                 telemetry.addData("Pose", format(pose));
-
-                /* We further illustrate how to decompose the pose into useful rotational and
-                 * translational components */
                 if (pose != null) {
                     VectorF trans = pose.getTranslation();
                     Orientation rot = Orientation.getOrientation(pose, AxesReference.EXTRINSIC, AxesOrder.XYZ, AngleUnit.DEGREES);
-
-                    // Extract the X, Y, and Z components of the offset of the target relative to the robot
                     double tX = trans.get(0);
                     double tY = trans.get(1);
                     double tZ = trans.get(2);
-
-                    // Extract the rotational components of the target relative to the robot
                     double rX = rot.firstAngle;
                     double rY = rot.secondAngle;
                     double rZ = rot.thirdAngle;
@@ -102,7 +84,7 @@ public class Red_TeleOp_1 extends LinearOpMode {
 
             telemetry.update();
         }
-
+        //run normal opmode
         ranVuforia = true;
         telemetry.addData("Vuforia Status:", ranVuforia);
 
