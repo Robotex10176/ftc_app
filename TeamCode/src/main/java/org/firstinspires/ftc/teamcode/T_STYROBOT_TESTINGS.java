3020ce3b0608c9e4701da1127ddbed09f2f8a766
Eric D'Urso <eric@casadurso.com>
2017-09-18 16:55:02 -0400

    Working on Gyro, need to test to se what values we need / want

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/T_STYROBOT_TESTINGS.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/T_STYROBOT_TESTINGS.java
@@ -10,11 +10,25 @@ import com.qualcomm.robotcore.hardware.IntegratingGyroscope;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
+import org.firstinspires.ftc.robotcore.external.ClassFactory;
+import org.firstinspires.ftc.robotcore.external.matrices.OpenGLMatrix;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackable;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
+
 /**
  * Created by Eric D'Urso on 9/17/2017.
  */
 
 public class T_STYROBOT_TESTINGS extends LinearOpMode {
+
+    //VUFORIA BUILD
+    boolean ranVuforia = false;
+    public static final String TAG = "Vuforia VuMark Sample";
+    OpenGLMatrix lastLocation = null;
+    VuforiaLocalizer vuforia;
+    //.
+
     private DcMotor andyMarkWithEncoder1;
     private DcMotor andyMarkWithEncoder2;
     private DcMotor tetrixWithEncoder;
@@ -24,7 +38,7 @@ public class T_STYROBOT_TESTINGS extends LinearOpMode {
     private Servo S3;
     private Servo S360;
     private ColorSensor ColorSensor;
-    //IDK Y
+    //IDK Y there be two of dem, copied
     IntegratingGyroscope gyro;
     ModernRoboticsI2cGyro modernRoboticsI2cGyro;
 
@@ -33,7 +47,6 @@ public class T_STYROBOT_TESTINGS extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
 
-        //Gyro Stuff
         boolean lastResetState = false;
         boolean curResetState  = false;
 
@@ -49,8 +62,37 @@ public class T_STYROBOT_TESTINGS extends LinearOpMode {
         modernRoboticsI2cGyro = hardwareMap.get(ModernRoboticsI2cGyro.class, "gyro");
         gyro = (IntegratingGyroscope)modernRoboticsI2cGyro;
 
-        waitForStart();
+        calibrateGyro();
+
+        //Vuforia Init:
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters(cameraMonitorViewId);//leave parameters blank to not display on phone
+        parameters.vuforiaLicenseKey = "AV6yugj/////AAAAGTOHqL6RDUmVgo0jZreKdLgqXGK+wd8vPtaDUOeepBzJahj4mF1oh/urYHvdw40evwj26RACNoqaxJWb1nS9RCaPjg25pDCZJJgFNSmtPHBU+f5AN1Y7ZJbJjNOAg8XvkX99ixa/gD/9HO9Es11cXjv0GkJof4M3ynaDqrh8S18dT5XT8QReygM64YyWkrsqjWI5H7WqZkuBDCSfmq0MVQiQrF9LChxd3/dTjChBJvcD8Rud19FEvu5IXq/Xem4KpPtuWDQAH0gWKJve8AzlcQLomY2nKtjbpcrZLpVjwtoo+C8NCCL5ng14uRCI8eriEg3OFD6v4ZNSZmbZIcUqAuX4YtFQG3t1RL0MT+3fWsBf";
+        parameters.cameraDirection = VuforiaLocalizer.CameraDirection.BACK;//change to front to switch camera used
+        this.vuforia = ClassFactory.createVuforiaLocalizer(parameters);
+        VuforiaTrackables relicTrackables = this.vuforia.loadTrackablesFromAsset("RelicVuMark");
+        VuforiaTrackable relicTemplate = relicTrackables.get(0);
+        relicTemplate.setName("relicVuMarkTemplate"); // can help in debugging; otherwise not necessary
+        telemetry.addData(">", "Press Play to start");
+        telemetry.update();
+        //.
+
+        waitForStart();//________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________-        relicTrackables.activate();
 
     }
+     public void calibrateGyro(){
+
+         telemetry.log().add("Gyro Calibrating. Do Not Move!");
+         modernRoboticsI2cGyro.calibrate();
+
+         timer.reset();
+         while (!isStopRequested() && modernRoboticsI2cGyro.isCalibrating())  {
+             telemetry.addData("calibrating", "%s", Math.round(timer.seconds())%2==0 ? "|.." : "..|");
+             telemetry.update();
+             sleep(50);
+         }
+
+         telemetry.log().clear(); telemetry.log().add("Gyro Calibrated. Press Start.");
+         telemetry.clear(); telemetry.update();
+     }
 }
- 
