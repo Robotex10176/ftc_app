e2689bfd8411a0e7a73269ede1db82eda44402e8
Eric D'Urso <eric@casadurso.com>
2017-09-25 16:59:30 -0400

    FInished Jewl Test Code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/T_PARTS_Jewl_Test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/T_PARTS_Jewl_Test.java
@@ -6,9 +6,10 @@ import com.qualcomm.robotcore.hardware.Servo;
 
 /**
  * Created by Eric D'Urso on 9/24/2017.
+ * NOTE THAT WE ARE RED
  */
 @Autonomous (name = "JEWL TEST")
-public class T_Jewl_Test extends LinearOpMode {
+public class T_PARTS_Jewl_Test extends LinearOpMode {
     private Servo flick;
     private Servo moveFlick;
     private com.qualcomm.robotcore.hardware.ColorSensor ColorSensor;
@@ -18,8 +19,28 @@ public class T_Jewl_Test extends LinearOpMode {
         flick = hardwareMap.servo.get("S2");
         moveFlick = hardwareMap.servo.get("S1");
         flick.setPosition(0.5);
-        moveFlick.setPosition(1);
+        sleep(1000);
+        moveFlick.setPosition(0);
+        sleep(1000);
         waitForStart();
+        moveFlick.setPosition(1);
+        sleep(1000);
+        if (ColorSensor.red()>ColorSensor.blue()){
+            flick.setPosition(1);
+            sleep(1000);
+        }
+        if (ColorSensor.red()<ColorSensor.blue()){
+            flick.setPosition(0);
+            sleep(1000);
+        }
+        else{
+            flick.setPosition(0.5);
+            sleep(1000);
+        }
+        flick.setPosition(0.5);
+        sleep(1000);
+        moveFlick.setPosition(0);
+        sleep(1000);
 
     }
 }
