1c27f328e937f091259288d92363343fa0242e43
Eric D'Urso <eric@casadurso.com>
2017-10-07 16:18:25 -0400

    Made Dumb Drive and Gyro Drive, Renamed to more all including name

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/T_Drive_Straight.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/T_Drive_Straight.java
@@ -11,18 +11,21 @@ import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
  * Created by Eric D'Urso on 10/6/2017.
  */
 
-public class T_Gyro_Drive extends LinearOpMode {
+public class T_Drive_Straight extends LinearOpMode {
     H_RobotHardware robot = new H_RobotHardware();
 
     @Override
     public void runOpMode() throws InterruptedException {
         robot.init(hardwareMap);
         waitForStart();
-        Drive(10, 0.1, 0.1);
+        //TEST ALL OF THE FOLLOWING METHODS FOR DRIVING STRAIGHT
+        driveKeepCheckingEncoders(10, 0.1, 0.1);
         //driveWithEncoderCheckAfterXAmountOfTime(10, 0.1, 0.1, 250);//250 = 1/4 of a second
+        //dumbDrive(10, 0.1, 0.1);
+        //gyroDrive(10, 0.1, 0.1);
     }
 
-    public void Drive(double DesiredDistance, double RightPower, double LeftPower) {
+    public void driveKeepCheckingEncoders(double DesiredDistance, double RightPower, double LeftPower) {
         double dg10incrs = 0.01;
         double dg22incrs = 0.05;
         double dg45incrs = 0.1;
@@ -118,6 +121,69 @@ public class T_Gyro_Drive extends LinearOpMode {
                 }
                 sleep(SleepTime);
                 idle();
+            }
+        }
+    }
+    public void gyroDrive(double DesiredDistance, double RightPower, double LeftPower){
+        double dg10incrs = 0.01;
+        double dg22incrs = 0.05;
+        double dg45incrs = 0.1;
+        float DesiredAngle = 0;
+        int newLeftTarget;
+        int newRightTarget;
+        float displacement;
+        final double COUNTS_PER_MOTOR_REV = 1120;    // TETRIX MOTORS = 1440, andymark = 1120
+        final double DRIVE_GEAR_REDUCTION = 1.0;     // This is < 1.0 if geared UP
+        final double WHEEL_DIAMETER_INCHES = 3.8125;     // For figuring circumference
+        final double COUNTS_PER_INCH = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) / (WHEEL_DIAMETER_INCHES * 3.1415);
+        if (opModeIsActive()) {
+            newLeftTarget = robot.leftDrive.getCurrentPosition() + (int) (DesiredDistance * COUNTS_PER_INCH);
+            newRightTarget = robot.rightDrive.getCurrentPosition() + (int) (DesiredDistance * COUNTS_PER_INCH);
+            robot.leftDrive.setTargetPosition(newLeftTarget);
+            robot.rightDrive.setTargetPosition(newRightTarget);
+            robot.leftDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            robot.rightDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            robot.leftDrive.setPower(Math.abs(LeftPower));
+            robot.rightDrive.setPower(Math.abs(RightPower));
+            while (opModeIsActive() && (robot.leftDrive.isBusy() && robot.rightDrive.isBusy())) {
+                float zAngle = robot.gyro.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES).firstAngle;
+                if ((zAngle > DesiredAngle)&& (LeftPower < 0.15)){
+                    LeftPower = LeftPower + 0.005;
+                } else if ((zAngle > DesiredAngle)&& (LeftPower > 0.15)){
+                    RightPower = RightPower - 0.005;
+                }
+                if ((zAngle < DesiredAngle)&& (LeftPower < 0.15)){
+                    RightPower = RightPower + 0.005;
+                } else if ((zAngle < DesiredAngle)&& (LeftPower > 0.15)){
+                    LeftPower = LeftPower - 0.005;
+                }
+            }
+        }
+    }
+    public void dumbDrive (double DesiredDistance, double RightPower, double LeftPower){
+        double dg10incrs = 0.01;
+        double dg22incrs = 0.05;
+        double dg45incrs = 0.1;
+        float zAngle = robot.gyro.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES).firstAngle;
+        float DesiredAngle = zAngle;
+        int newLeftTarget;
+        int newRightTarget;
+        float displacement;
+        final double COUNTS_PER_MOTOR_REV = 1120;    // TETRIX MOTORS = 1440, andymark = 1120
+        final double DRIVE_GEAR_REDUCTION = 1.0;     // This is < 1.0 if geared UP
+        final double WHEEL_DIAMETER_INCHES = 3.8125;     // For figuring circumference
+        final double COUNTS_PER_INCH = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) / (WHEEL_DIAMETER_INCHES * 3.1415);
+        if (opModeIsActive()) {
+            newLeftTarget = robot.leftDrive.getCurrentPosition() + (int) (DesiredDistance * COUNTS_PER_INCH);
+            newRightTarget = robot.rightDrive.getCurrentPosition() + (int) (DesiredDistance * COUNTS_PER_INCH);
+            robot.leftDrive.setTargetPosition(newLeftTarget);
+            robot.rightDrive.setTargetPosition(newRightTarget);
+            robot.leftDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            robot.rightDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            robot.leftDrive.setPower(Math.abs(LeftPower));
+            robot.rightDrive.setPower(Math.abs(RightPower));
+            while (opModeIsActive() && (robot.leftDrive.isBusy() && robot.rightDrive.isBusy())) {
+// DO NOTHING BECUZ THIS IS DUMB
             }
         }
     }
