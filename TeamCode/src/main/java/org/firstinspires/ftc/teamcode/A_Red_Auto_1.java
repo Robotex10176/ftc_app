7a5cf1c0897e83ad506e26faca72f9cc118d9f56
David Reeves <djreeves2005@gmail.com>
2017-10-07 15:14:24 -0400

    Visual Studio Code Test
    + Java Class Accessable!

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/A_Red_Auto_1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/A_Red_Auto_1.java
@@ -50,9 +50,9 @@ public class A_Red_Auto_1 extends LinearOpMode {
         telemetry.addLine("Closing Claw in 4");
         telemetry.update();
         sleep(1000);
-        telemetry.addLine("Playing Warning Sound!!!");
+        telemetry.addLine("Playing Warning Sound");
         telemetry.update();
-        //Warning Sound
+        //Warning Sound Goes Here
         telemetry.addLine("Closing Claw in 3");
         telemetry.update();
         sleep(1000);
