7457cb1de88c0c94e0255d84ed37ab25e66f7571
Eric D'Urso <eric@casadurso.com>
2017-10-07 15:35:53 -0400

    Made Program, needs to be exponential

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/T_Gyro_Drive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/T_Gyro_Drive.java
@@ -13,13 +13,15 @@ import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
 
 public class T_Gyro_Drive extends LinearOpMode {
     H_RobotHardware robot = new H_RobotHardware();
+
     @Override
     public void runOpMode() throws InterruptedException {
         robot.init(hardwareMap);
         waitForStart();
         Drive(10, 0.1, 0.1);
     }
-    public void Drive(double DesiredDistance, double RightPower, double LeftPower){
+
+    public void Drive(double DesiredDistance, double RightPower, double LeftPower) {
         double dg10incrs = 0.01;
         double dg22incrs = 0.05;
         double dg45incrs = 0.1;
@@ -27,14 +29,22 @@ public class T_Gyro_Drive extends LinearOpMode {
         float DesiredAngle = zAngle;
         int newLeftTarget;
         int newRightTarget;
-        float Displacement;
-        final double     COUNTS_PER_MOTOR_REV    = 1120 ;    // TETRIX MOTORS = 1440, andymark = 1120
-        final double     DRIVE_GEAR_REDUCTION    = 1.0 ;     // This is < 1.0 if geared UP
-        final double     WHEEL_DIAMETER_INCHES   = 3.8125 ;     // For figuring circumference
-        final double     COUNTS_PER_INCH         = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) / (WHEEL_DIAMETER_INCHES * 3.1415);
-        if (opModeIsActive()){
-            newLeftTarget = robot.leftDrive.getCurrentPosition() + (int)(DesiredDistance * COUNTS_PER_INCH);
-            newRightTarget = robot.rightDrive.getCurrentPosition() + (int)(DesiredDistance * COUNTS_PER_INCH);
+        int startPositionR;
+        int startPositionL;
+        int currentPositionRight;
+        int currentPositionLeft;
+        int rightPositionTraveled;
+        int leftPositionTraveled;
+        float displacement;
+        final double COUNTS_PER_MOTOR_REV = 1120;    // TETRIX MOTORS = 1440, andymark = 1120
+        final double DRIVE_GEAR_REDUCTION = 1.0;     // This is < 1.0 if geared UP
+        final double WHEEL_DIAMETER_INCHES = 3.8125;     // For figuring circumference
+        final double COUNTS_PER_INCH = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) / (WHEEL_DIAMETER_INCHES * 3.1415);
+        if (opModeIsActive()) {
+            startPositionR = robot.rightDrive.getCurrentPosition();
+            startPositionL = robot.leftDrive.getCurrentPosition();
+            newLeftTarget = robot.leftDrive.getCurrentPosition() + (int) (DesiredDistance * COUNTS_PER_INCH);
+            newRightTarget = robot.rightDrive.getCurrentPosition() + (int) (DesiredDistance * COUNTS_PER_INCH);
             robot.leftDrive.setTargetPosition(newLeftTarget);
             robot.rightDrive.setTargetPosition(newRightTarget);
             robot.leftDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
@@ -42,11 +52,19 @@ public class T_Gyro_Drive extends LinearOpMode {
             robot.leftDrive.setPower(Math.abs(LeftPower));
             robot.rightDrive.setPower(Math.abs(RightPower));
             while (opModeIsActive() && (robot.leftDrive.isBusy() && robot.rightDrive.isBusy())) {
-                Displacement = DesiredAngle - zAngle;
-                if (Displacement > DesiredAngle){
-                    //stop right for x amount of ticks
+                currentPositionRight = robot.rightDrive.getCurrentPosition();
+                currentPositionLeft = robot.leftDrive.getCurrentPosition();
+                rightPositionTraveled = currentPositionRight - startPositionR;
+                leftPositionTraveled = currentPositionLeft - startPositionL;
+                if (rightPositionTraveled > leftPositionTraveled){
+                    LeftPower = LeftPower + 0.01;
+                }
+                if (rightPositionTraveled < leftPositionTraveled){
+                    RightPower = RightPower + 0.01;
                 }
             }
         }
     }
 }
+
+
