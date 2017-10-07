051be5ecd6651fbf5091e2ba40d7773fd06b1f67
Eric D'Urso <eric@casadurso.com>
2017-10-07 16:00:29 -0400

    Made Second method to only check encoders after X amount of time

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/T_Gyro_Drive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/T_Gyro_Drive.java
@@ -19,6 +19,7 @@ public class T_Gyro_Drive extends LinearOpMode {
         robot.init(hardwareMap);
         waitForStart();
         Drive(10, 0.1, 0.1);
+        //driveWithEncoderCheckAfterXAmountOfTime(10, 0.1, 0.1, 250);//250 = 1/4 of a second
     }
 
     public void Drive(double DesiredDistance, double RightPower, double LeftPower) {
@@ -56,12 +57,67 @@ public class T_Gyro_Drive extends LinearOpMode {
                 currentPositionLeft = robot.leftDrive.getCurrentPosition();
                 rightPositionTraveled = currentPositionRight - startPositionR;
                 leftPositionTraveled = currentPositionLeft - startPositionL;
-                if (rightPositionTraveled > leftPositionTraveled){
-                    LeftPower = LeftPower + 0.01;
+                if ((rightPositionTraveled > leftPositionTraveled)&& (LeftPower < 0.15)){
+                    LeftPower = LeftPower + 0.005;
+                } else if ((rightPositionTraveled > leftPositionTraveled)&& (LeftPower > 0.15)){
+                    RightPower = RightPower - 0.005;
                 }
-                if (rightPositionTraveled < leftPositionTraveled){
-                    RightPower = RightPower + 0.01;
+                if ((rightPositionTraveled < leftPositionTraveled) && (RightPower < 0.15)){
+                    RightPower = RightPower + 0.005;
+                } else if ((rightPositionTraveled < leftPositionTraveled)&& (RightPower > 0.15)){
+                    LeftPower = LeftPower - 0.005;
                 }
+                idle();
+            }
+        }
+    }
+    public void driveWithEncoderCheckAfterXAmountOfTime (double DesiredDistance, double RightPower, double LeftPower, int SleepTime){
+        double dg10incrs = 0.01;
+        double dg22incrs = 0.05;
+        double dg45incrs = 0.1;
+        float zAngle = robot.gyro.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES).firstAngle;
+        float DesiredAngle = zAngle;
+        int newLeftTarget;
+        int newRightTarget;
+        int startPositionR;
+        int startPositionL;
+        int currentPositionRight;
+        int currentPositionLeft;
+        int rightPositionTraveled;
+        int leftPositionTraveled;
+        float displacement;
+        final double COUNTS_PER_MOTOR_REV = 1120;    // TETRIX MOTORS = 1440, andymark = 1120
+        final double DRIVE_GEAR_REDUCTION = 1.0;     // This is < 1.0 if geared UP
+        final double WHEEL_DIAMETER_INCHES = 3.8125;     // For figuring circumference
+        final double COUNTS_PER_INCH = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) / (WHEEL_DIAMETER_INCHES * 3.1415);
+        if (opModeIsActive()) {
+            startPositionR = robot.rightDrive.getCurrentPosition();
+            startPositionL = robot.leftDrive.getCurrentPosition();
+            newLeftTarget = robot.leftDrive.getCurrentPosition() + (int) (DesiredDistance * COUNTS_PER_INCH);
+            newRightTarget = robot.rightDrive.getCurrentPosition() + (int) (DesiredDistance * COUNTS_PER_INCH);
+            robot.leftDrive.setTargetPosition(newLeftTarget);
+            robot.rightDrive.setTargetPosition(newRightTarget);
+            robot.leftDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            robot.rightDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            robot.leftDrive.setPower(Math.abs(LeftPower));
+            robot.rightDrive.setPower(Math.abs(RightPower));
+            while (opModeIsActive() && (robot.leftDrive.isBusy() && robot.rightDrive.isBusy())) {
+                currentPositionRight = robot.rightDrive.getCurrentPosition();
+                currentPositionLeft = robot.leftDrive.getCurrentPosition();
+                rightPositionTraveled = currentPositionRight - startPositionR;
+                leftPositionTraveled = currentPositionLeft - startPositionL;
+                if ((rightPositionTraveled > leftPositionTraveled)&& (LeftPower < 0.15)){
+                    LeftPower = LeftPower + 0.005;
+                } else if ((rightPositionTraveled > leftPositionTraveled)&& (LeftPower > 0.15)){
+                    RightPower = RightPower - 0.005;
+                }
+                if ((rightPositionTraveled < leftPositionTraveled) && (RightPower < 0.15)){
+                    RightPower = RightPower + 0.005;
+                } else if ((rightPositionTraveled < leftPositionTraveled)&& (RightPower > 0.15)){
+                    LeftPower = LeftPower - 0.005;
+                }
+                sleep(SleepTime);
+                idle();
             }
         }
     }
