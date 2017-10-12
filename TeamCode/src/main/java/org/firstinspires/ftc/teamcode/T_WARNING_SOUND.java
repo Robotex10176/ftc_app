55a38c1b1e0dd7caaffb2392030756e4f8a9440b
hacker <github-f@standland.org>
2017-10-12 18:01:31 -0400

    deleted code that didn't work

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/T_WARNING_SOUND.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/T_WARNING_SOUND.java
@@ -1,2 +1,3 @@
 //Insert Sound Code Here
+
 //Used in A_Red_Auto_1.java code where the claw is closing for initilization
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/T_taking_pictures.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/T_taking_pictures.java
@@ -1,47 +1,14 @@
-/**
- *
 
-*package org.firstinspires.ftc.teamcode;
-*import android.hardware.Camera;
-*import android.hardware.camera2;
-*import android.hardware.camera;
-*import android.widget.FrameLayout;
+package org.firstinspires.ftc.teamcode;
+import android.hardware.Camera;
+import android.hardware.camera2;
+import android.hardware.camera;
+import android.widget.FrameLayout;
 
-*import org.firstinspires.ftc.robotcontroller.internal.FtcRobotControllerActivity;
+import org.firstinspires.ftc.robotcontroller.internal.FtcRobotControllerActivity;
 
 
-*public class T_taking_pictures {
+public class T_taking_pictures {
 //this is where I will do research on the photos for the jewel identifier thing
-public Camera camera;
-    private Camera openFrontFacingCamera() {
-        int cameraId = -1;
-        Camera cam = null;
-        int numberOfCameras = Camera.getNumberOfCameras();
-        for (int i = 0; i < numberOfCameras; i++) {
-            Camera.CameraInfo info = new Camera.CameraInfo();
-            Camera.getCameraInfo(i, info);
-            if (info.facing == Camera.CameraInfo.CAMERA_FACING_FRONT) {
-                cameraId = i;
-                break;
-            }
-        }
-        try {
-            cam = Camera.open(cameraId);
-        } catch (Exception e) {
+}
 
-        }
-        return cam;
-    }
-
-    public void initPreview(final Camera camera, final CameraOp context, final Camera.PreviewCallback previewCallback) {
-        runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                context.preview = new CameraPreview(FtcRobotControllerActivity.this, camera, previewCallback);
-                FrameLayout previewLayout = (FrameLayout) findViewById(R.id.previewLayout);
-                previewLayout.addView(context.preview);
-            }
-        });
-    }
-*}
-*/
