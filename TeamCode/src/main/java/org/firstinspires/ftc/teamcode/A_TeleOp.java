a86fc33a38d95c222f5c177a185facfc28edc8c9
Eric D'Urso <eric@casadurso.com>
2017-10-16 17:21:40 -0400

    Don't Worry, Ive put the methods all into the hardware class, just need to name and call correctly

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/A_TeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/A_TeleOp.java
@@ -14,139 +14,35 @@ import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
 @TeleOp(name = "Main TeleOp")
 public class A_TeleOp extends OpMode {
     H_RobotHardware robot = new H_RobotHardware();
-    private float scaleController(float in){
-        //return ( java.lang.Math.signum(in)*in*in);
-        return (in*in*in);
-    }
-    boolean A = true;
     @Override
     public void init() {
-        robot.init(hardwareMap, false );
+        robot.init(hardwareMap, false);
     }
     @Override
     public void loop() {
         //robot.rightDrive.setPower(scaleController(gamepad1.right_stick_y));
         //robot.leftDrive.setPower(scaleController(gamepad1.left_stick_y));
-        if ((gamepad1.left_trigger < 0.05) || (gamepad1.right_trigger < 0.05)){
+        if ((gamepad1.left_trigger < 0.05) || (gamepad1.right_trigger < 0.05)) {
             robot.rightDrive.setPower(scaleController(-gamepad1.right_stick_y));
             robot.leftDrive.setPower(scaleController(-gamepad1.left_stick_y));
         }
-        if ((gamepad1.left_trigger > 0.05) || (gamepad1.right_trigger > 0.05)){
-            robot.rightDrive.setPower(scaleController((gamepad1.right_trigger)/2));
-            robot.leftDrive.setPower(scaleController((gamepad1.left_trigger)/2));
+        if ((gamepad1.left_trigger > 0.05) || (gamepad1.right_trigger > 0.05)) {
+            robot.rightDrive.setPower(scaleController((gamepad1.right_trigger) / 2));
+            robot.leftDrive.setPower(scaleController((gamepad1.left_trigger) / 2));
         }
-        if(gamepad1.dpad_up){
+        if (gamepad1.dpad_up) {
             robot.Lift.setPower(0.3);
         }
-        if(gamepad1.dpad_down){
+        if (gamepad1.dpad_down) {
             robot.Lift.setPower(-0.3);
         }
-        if (((!gamepad1.dpad_up) &&(!gamepad1.dpad_down)) ){
+        if (((!gamepad1.dpad_up) && (!gamepad1.dpad_down))) {
             robot.Lift.setPower(0);
         }
-        if (gamepad1.left_bumper){
+        if (gamepad1.left_bumper) {
             CloseClaw();
-        } else{
+        } else {
             OpenClaw();
         }
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    //oops
-
-
-
-    public void Turn(double Angle, double Power){
-        //code to turn untill an angle ex 0, 90, -90
-        float zAngle;
-        zAngle = robot.gyro.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES).firstAngle;
-        if (Angle < 0){
-            while (zAngle != Angle){
-                robot.leftDrive.setPower(Power);
-                robot.rightDrive.setPower(-Power);
-                telemetry.addData("Angle:", zAngle);
-                telemetry.update();
-                zAngle = robot.gyro.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES).firstAngle;
-
-            }
-        } else if (Angle > 0){
-            while (zAngle != Angle){
-                robot.leftDrive.setPower(-Power);
-                robot.rightDrive.setPower(Power);
-                telemetry.addData("Angle:", zAngle);
-                telemetry.update();
-                zAngle = robot.gyro.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES).firstAngle;
-
-            }
-        }
-    }
-    public void CloseClaw (){
-        robot.RightClaw.setPosition(0.75);
-        robot.LeftClaw.setPosition(0.50);
-    }
-    public void OpenClaw (){
-        robot.RightClaw.setPosition(1);
-        robot.LeftClaw.setPosition(0.25);
-    }
-    public void FlatClaw(){
-        robot.RightClaw.setPosition(0.3);
-        robot.LeftClaw.setPosition(0);
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/H_RobotHardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/H_RobotHardware.java
@@ -88,5 +88,167 @@ public class H_RobotHardware
         }
 
     }
+    //ODD NOT ROBOT MOVIN METHODS BELOW
+
+    public float AngularSeparation (float a, float b){
+        float rv;
+        rv = Math.abs(a-b);
+        if (rv >= 180){
+            rv = 360 - rv;
+        }
+        return rv;
+    }
+
+    private float scaleController(float in){
+        //return ( java.lang.Math.signum(in)*in*in);
+        return (in*in*in);
+    }
+
+    //TEAM METHODS BELOW
+
+    public void SmartTurnRight(float Angle, double Power){//turn right is clockwise
+        //code to turn untill an angle ex 0, 90, -90
+        float zAngle;
+        float targetAngle;
+        zAngle = gyro.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES).firstAngle;
+        //Set target direction in range -180 - 180;
+        targetAngle = (zAngle - Angle + 180);
+        while (targetAngle > 360){ targetAngle = targetAngle - 360; }
+        while (targetAngle < 0){ targetAngle = targetAngle + 360; }
+        targetAngle = targetAngle - 180;
+
+        while (AngularSeparation(zAngle, targetAngle)> 2.0){
+            leftDrive.setPower(Power);
+            rightDrive.setPower(-Power);
+            zAngle = gyro.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES).firstAngle;
+        }
+        leftDrive.setPower(0);
+        rightDrive.setPower(0);
+
+    }
+
+    public void SmartTurnLeft (float Angle, double Power){
+        float zAngle;
+        float targetAngle;
+        zAngle = gyro.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES).firstAngle;
+        //Set target direction in range -180 - 180;
+        targetAngle = (zAngle - Angle + 180);
+        while (targetAngle > 360){ targetAngle = targetAngle - 360; }
+        while (targetAngle < 0){ targetAngle = targetAngle + 360; }
+        targetAngle = targetAngle - 180;
+
+        while (AngularSeparation(zAngle, targetAngle)> 2.0){
+            leftDrive.setPower(-Power);
+            rightDrive.setPower(Power);
+            zAngle = gyro.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES).firstAngle;
+
+        }
+        leftDrive.setPower(0);
+        rightDrive.setPower(0);
+
+    }
+    public void CloseClaw (){
+        RightClaw.setPosition(0.75);
+        LeftClaw.setPosition(0.50);
+    }
+    public void OpenClaw (){
+        RightClaw.setPosition(1);
+        LeftClaw.setPosition(0.25);
+    }
+    public void FlatClaw(){
+        RightClaw.setPosition(0.3);
+        LeftClaw.setPosition(0);
+    }
+    public void moveArm (double Degrees, double Power){
+        int newLeftTarget;
+        final double COUNTS_PER_MOTOR_REV = 1120;    // TETRIX MOTORS = 1440, andymark = 1120
+        final double DEGREES = (Degrees*(COUNTS_PER_MOTOR_REV/360));//3.111111111111111111111111111 ticks per degree
+        if (A) {
+            newLeftTarget = arm.getCurrentPosition() + (int) (DEGREES);
+            arm.setTargetPosition(newLeftTarget);
+            arm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            arm.setPower(Math.abs(Power));
+            while (A && (arm.isBusy())) {
+
+            }
+        }
+    }
+    public void SeeOurColor (){
+        moveFlick.setPosition(0);
+    }
+    public void DontSeeOurColor (){
+        moveFlick.setPosition(1);
+    }
+    public void JewelServoReturn (double KNOW_THAT_YOU_HAVE_TO_SLEEP_BEFORE_THIS){
+        moveFlick.setPosition(0.5);
+    }
+    public void DriveNoCorrection (double DesiredDistance, double RightPower, double LeftPower){
+        int newLeftTarget;
+        int newRightTarget;
+        final double COUNTS_PER_MOTOR_REV = 1440;    // TETRIX MOTORS = 1440, andymark = 1120
+        final double DRIVE_GEAR_REDUCTION = 1.0;     // This is < 1.0 if geared UP
+        final double WHEEL_DIAMETER_INCHES = 3.8125;     // For figuring circumference
+        final double COUNTS_PER_INCH = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) / (WHEEL_DIAMETER_INCHES * 3.1415);
+        if (A) {
+            newLeftTarget = leftDrive.getCurrentPosition() + (int) (DesiredDistance * COUNTS_PER_INCH);
+            newRightTarget = rightDrive.getCurrentPosition() + (int) (DesiredDistance * COUNTS_PER_INCH);
+            leftDrive.setTargetPosition(newLeftTarget);
+            rightDrive.setTargetPosition(newRightTarget);
+            leftDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            rightDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            leftDrive.setPower(Math.abs(LeftPower));
+            rightDrive.setPower(Math.abs(RightPower));
+            while (A && (leftDrive.isBusy() && rightDrive.isBusy())) {
+// DO NOTHING BECUZ THIS IS DUMB
+
+            }
+            rightDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            leftDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            rightDrive.setPower(0);
+            leftDrive.setPower(0);
+            rightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            leftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        }
+    }
+    public void DriveGyroCorrection(double DesiredDistance, double RightPower, double LeftPower){
+        double dg10incrs = 0.01;
+        double dg22incrs = 0.05;
+        double dg45incrs = 0.1;
+        float DesiredAngle = 0;
+        int newLeftTarget;
+        int newRightTarget;
+        float displacement;
+        final double COUNTS_PER_MOTOR_REV = 1440;    // TETRIX MOTORS = 1440, andymark = 1120
+        final double DRIVE_GEAR_REDUCTION = 1.0;     // This is < 1.0 if geared UP
+        final double WHEEL_DIAMETER_INCHES = 3.8125;     // For figuring circumference
+        final double COUNTS_PER_INCH = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) / (WHEEL_DIAMETER_INCHES * 3.1415);
+        if (A) {
+            newLeftTarget = leftDrive.getCurrentPosition() + (int) (DesiredDistance * COUNTS_PER_INCH);
+            newRightTarget = rightDrive.getCurrentPosition() + (int) (DesiredDistance * COUNTS_PER_INCH);
+            leftDrive.setTargetPosition(newLeftTarget);
+            rightDrive.setTargetPosition(newRightTarget);
+            leftDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            rightDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            leftDrive.setPower(Math.abs(LeftPower));
+            rightDrive.setPower(Math.abs(RightPower));
+            while (A && (leftDrive.isBusy() && rightDrive.isBusy())) {
+                float zAngle = gyro.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES).firstAngle;
+                if (zAngle > DesiredAngle){
+                    LeftPower = LeftPower + ((zAngle - DesiredAngle)/90) * 0.05;
+                    RightPower = RightPower - ((zAngle - DesiredAngle)/90) * 0.05;
+                } else {
+                    LeftPower = LeftPower - (-1*(zAngle - DesiredAngle)/90) * 0.05;
+                    RightPower = RightPower + (-1*(zAngle - DesiredAngle)/90) * 0.05;
+                }
+                leftDrive.setPower(Math.abs(LeftPower));
+                rightDrive.setPower(Math.abs(RightPower));
+            }
+            float zAngle = gyro.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES).firstAngle;
+            if (zAngle != DesiredAngle){
+                leftDrive.setPower(-0.05);
+                leftDrive.setPower(0.05);
+            }
+        }
+    }
  }
 
