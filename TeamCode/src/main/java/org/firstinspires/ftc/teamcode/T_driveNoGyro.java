ac1b3a98e2b7bf780112e4be26242e25e4513133
hacker <github-f@standland.org>
2017-11-22 14:22:20 -0500

    fixed code and failed to compile :(

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/T_driveNoGyro.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/T_driveNoGyro.java
@@ -7,12 +7,19 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+
+
+
 @Autonomous(name = "DRIVE NO GYRO")
 public class T_driveNoGyro extends LinearOpMode {
     A_Main robot = new A_Main();
+
     @Override
-    robot.init(hardwareMap, true);
-    waitForStart();
+    public void runOpMode() throws InterruptedException {
+        robot.init(hardwareMap, true);
+
+        waitForStart();
         //public void DriveNoCorrection(double DesiredDistance, double RightPower, double LeftPower) {
         DriveNoCorrection(1, 10, 10);
     }
+}
