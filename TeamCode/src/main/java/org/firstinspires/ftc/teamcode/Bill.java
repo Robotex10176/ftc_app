6940fb73e7555761fe8374d9f8e72a702973a02d
Unknown <github-f@standland.org>
2018-03-11 15:59:55 -0400

    added code to BILL

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Bill.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Bill.java
@@ -1,8 +1,25 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+
 /**
  * Created by daviado on 3/11/18.
  */
 
-public class Bill {
+@Autonomous(name = "Bill_test")
+public class Bill extends LinearOpMode {
+    //Hardware Declaration
+    public DcMotor gray;
+    public void runOpMode() throws InterruptedException {
+        //Hardware Initalization
+        gray = hardwareMap.dcMotor.get("gray");
+        waitForStart();
+        //Hardware be like doo stuff
+        //moov arm
+        while (opModeIsActive){
+            gray.setPower(1);
+        }
+    }
 }
